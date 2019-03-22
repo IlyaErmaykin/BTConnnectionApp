f9ec6ae53119d8a047ef068aeba80b42e033190a
User_RITG <developer@ritg.ru>
2019-03-22 20:12:07 +0400

    #1 BTConnectionApp;  - Add BTConnectionApp;

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTConnectionApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Default main function
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            StartLogic startLogic = new StartLogic();
+
+
+        }
+    }
+}
---
+++ StartLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTConnectionApp
+{
+    /// <summary>
+    /// Start logic functon
+    /// </summary>
+    class StartLogic
+    {
+        //Bluetooth
+
+        //// проверяем, что у нас получен адаптер (есть Bluetooth) и он включен
+        //if (adapter != null && adapter.isEnabled)
+        //{
+        //    // получим список связанных устройств
+        //    ICollection<BluetoothDevice> devices = adapter.BondedDevices;
+
+        //    // инициализируем пустой список строк имен устройств
+        //    List<String> names = new List<String>();
+
+        //    // пройдем по списку устройств и будем заполнять список имен
+        //    foreach (var device in devices)
+        //    {
+        //        // device - сопряженное устройство из BondedDevices
+        //        names.Add(device.Name); // добавление имени
+        //    }
+        //}
+
+
+    }
+}
